5e75873f33d2f0fd8c993f3e6802c531194eb2da
chriss <chriss@vandorensales.com>
2020-07-23 14:22:18 -0700

    initial commit

---
+++ public/background.js
@@ -0,0 +1,89 @@
+/*global chrome*/
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Installing background refresh...');
+    // create alarm after extension is installed / upgraded
+    chrome.alarms.create('refresh', { periodInMinutes: 1 });
+    fetchNotifications();
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'refresh') {
+        fetchNotifications();
+    }
+});
+
+const newNotificationsEvent = new Event('notification');
+function fetchNotifications() {
+    fetch('https://stackoverflow.com/topbar/inbox')
+      .then(res => res.text())
+      .then(res => {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(res, 'text/html');
+
+        const items = html.getElementsByClassName('inbox-item');
+        const unreadItems = html.getElementsByClassName('unread-item');
+
+        if (unreadItems.length > 0) {
+            chrome.browserAction.setBadgeText({ text: unreadItems.length.toString() });
+            chrome.browserAction.setBadgeBackgroundColor('red');
+            chrome.browserAction.setBadgeTextColor('white');
+        }
+
+        let notifications = [];
+
+        for (const item of items) {
+          const type = item.getElementsByClassName('item-type')[0];
+          const created = item.getElementsByClassName('item-creation')[0];
+          const title = item.getElementsByClassName('item-location')[0];
+          const comment = item.getElementsByClassName('item-summary')[0];
+          const aTag = item.getElementsByTagName('a')[0];
+
+          let unread = false;
+          let url = '';
+          let typeForNotif = '';
+          let dateForNotif = '';
+          let titleForNotif = '';
+          let commentForNotif = '';
+
+          if (item.classList.contains('unread-item')) {
+            unread = true;
+          }
+
+          if (type != null) {
+            let text = type.textContent;
+            text = text.replace('Stack Overflow', '');
+            typeForNotif = text.trim();
+          }
+
+          if (created != null) {
+            dateForNotif = created.textContent;
+          }
+
+          if (title != null) {
+            titleForNotif = title.textContent;
+          }
+
+          if (comment != null) {
+            commentForNotif = comment.textContent;
+          }
+
+          if (aTag != null) {
+            url = aTag.href;
+          }
+
+          notifications.push({
+            type: typeForNotif,
+            created: dateForNotif,
+            title: titleForNotif,
+            comment: commentForNotif,
+            source: url,
+            unread: unread
+          });
+        }
+
+        localStorage.setItem('notifications', JSON.stringify(notifications));
+
+        window.dispatchEvent(newNotificationsEvent);
+      });
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    setNotifications(JSON.parse(localStorage.getItem('notifications')));
+  }, []);
+
+  useEffect(() => {
+    function refreshView() {
+      setNotifications(JSON.parse(localStorage.getItem('notifications')));
+    }
+
+    window.addEventListener('notification', refreshView);
+
+    return () => window.removeEventListener('notification', refreshView);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { notifications.map((notif, key) => {
+          return (
+            <a
+              className="link-to-so"
+              href={notif.source}
+              rel="noopener noreferrer"
+              target="_blank"
+              key={key}
+              style={{
+                padding: 5,
+                background: notif.unread && '#344c63'
+              }}
+            >
+              <p style={{ display: 'flex', justifyContent: 'space-between', margin: 0, fontSize: 12, color: 'hsl(220, 13%, 68%)' }}>
+                <span>{ notif.type }</span>
+                <span>{ notif.created }</span>
+              </p>
+              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>{ notif.title }</p>
+              <p style={{ fontSize: 12, margin: 0 }}>{ notif.comment }</p>
+            </a>
+          );
+        })}
       </header>
     </div>
   );
