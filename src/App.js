1cd8b58cd073a25000274156af95ec1f476d94c4
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-24 02:55:49 -0400

    Full theming capability

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [notifications, setNotifications] = useState([]);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
   useEffect(() => {
-    setNotifications(JSON.parse(localStorage.getItem('notifications')));
+    setNotifications(JSON.parse(localStorage.getItem("notifications")));
     setLoading(false);
   }, []);
 
   useEffect(() => {
     function refreshView() {
-      setNotifications(JSON.parse(localStorage.getItem('notifications')));
+      setNotifications(JSON.parse(localStorage.getItem("notifications")));
     }
 
-    window.addEventListener('notification', refreshView);
+    window.addEventListener("notification", refreshView);
 
-    return () => window.removeEventListener('notification', refreshView);
+    return () => window.removeEventListener("notification", refreshView);
   }, []);
+
+  function handleTheme() {
+    if (theme === "dark") {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+    } else {
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        { loading && <h3>Loading Notifications...</h3> }
+      <header className={theme === "dark" ? "App-header" : "Light-header"}>
+        <div className="switch-container">
+          <label className="switch">
+            <input type="checkbox" onClick={handleTheme} />
+            <span className="slider round"></span>
+          </label>
+          <p className="theme-indicator">
+            <strong>{theme}</strong>
+          </p>
+        </div>
 
-        { !loading && notifications.length === 0 && <p>You have nothing in your inbox. Make sure you&apos;re logged in to Stack Overflow so this inbox populates.</p>}
+        {loading && <h3>Loading Notifications...</h3>}
 
-        { notifications.map((notif, key) => {
+        {!loading && notifications.length === 0 && (
+          <p>
+            You have nothing in your inbox. Make sure you&apos;re logged in to
+            Stack Overflow so this inbox populates.
+          </p>
+        )}
+
+        {notifications.map((notif, key) => {
           return (
             <a
               className="link-to-so"
@@ -36,12 +62,22 @@ function App() {
               target="_blank"
               key={key}
             >
-              <p style={{ display: 'flex', justifyContent: 'space-between', margin: 0, fontSize: 12, color: 'hsl(220, 13%, 68%)' }}>
-                <span>{ notif.type }</span>
-                <span>{ notif.created }</span>
+              <p
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  margin: 0,
+                  fontSize: 12,
+                  color: theme === "dark" ? "hsl(220, 13%, 68%)" : "black",
+                }}
+              >
+                <span>{notif.type}</span>
+                <span>{notif.created}</span>
               </p>
-              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>{ notif.title }</p>
-              <p style={{ fontSize: 12, margin: 0 }}>{ notif.comment }</p>
+              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>
+                {notif.title}
+              </p>
+              <p style={{ fontSize: 12, margin: 0 }}>{notif.comment}</p>
             </a>
           );
         })}
