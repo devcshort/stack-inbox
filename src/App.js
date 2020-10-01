c5c2ee3547f85ef325a3812a8781d839af3c2761
Bhavin789 <bhavib.789@gmail.com>
2020-10-01 22:13:19 +0530

    Add a null check to prevent React app from breaking

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    setNotifications(JSON.parse(localStorage.getItem('notifications')));
+    const localStorageNotifications = JSON.parse(
+      localStorage.getItem("notifications")
+    );
+    if (!localStorageNotifications) {
+      setNotifications([]);
+    } else {
+      setNotifications(localStorageNotifications);
+    }
     setLoading(false);
   }, []);
 
   useEffect(() => {
     function refreshView() {
-      setNotifications(JSON.parse(localStorage.getItem('notifications')));
+      setNotifications(JSON.parse(localStorage.getItem("notifications")));
     }
 
-    window.addEventListener('notification', refreshView);
+    window.addEventListener("notification", refreshView);
 
-    return () => window.removeEventListener('notification', refreshView);
+    return () => window.removeEventListener("notification", refreshView);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        { loading && <h3>Loading Notifications...</h3> }
+        {loading && <h3>Loading Notifications...</h3>}
 
-        { !loading && notifications.length === 0 && <p>You have nothing in your inbox. Make sure you&apos;re logged in to Stack Overflow so this inbox populates.</p>}
+        {!loading && notifications.length === 0 && (
+          <p>
+            You have nothing in your inbox. Make sure you&apos;re logged in to
+            Stack Overflow so this inbox populates.
+          </p>
+        )}
 
-        { notifications.map((notif, key) => {
+        {notifications.map((notif, key) => {
           return (
             <a
               className="link-to-so"
@@ -36,12 +48,22 @@ function App() {
               target="_blank"
               key={key}
             >
-              <p style={{ display: 'flex', justifyContent: 'space-between', margin: 0, fontSize: 12, color: 'hsl(220, 13%, 68%)' }}>
-                <span>{ notif.type }</span>
-                <span>{ notif.created }</span>
+              <p
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  margin: 0,
+                  fontSize: 12,
+                  color: "hsl(220, 13%, 68%)",
+                }}
+              >
+                <span>{notif.type}</span>
+                <span>{notif.created}</span>
               </p>
-              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>{ notif.title }</p>
-              <p style={{ fontSize: 12, margin: 0 }}>{ notif.comment }</p>
+              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>
+                {notif.title}
+              </p>
+              <p style={{ fontSize: 12, margin: 0 }}>{notif.comment}</p>
             </a>
           );
         })}
