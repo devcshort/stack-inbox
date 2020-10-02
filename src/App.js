59fa41b342dde63ac55c93062b010393c55a8b9c
Bhavin789 <bhavib.789@gmail.com>
2020-10-02 21:53:29 +0530

    Revert comma formatting changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     const localStorageNotifications = JSON.parse(
-      localStorage.getItem("notifications")
+      localStorage.getItem('notifications')
     );
     if (!localStorageNotifications) {
       setNotifications([]);
@@ -19,17 +19,17 @@ function App() {
 
   useEffect(() => {
     function refreshView() {
-      setNotifications(JSON.parse(localStorage.getItem("notifications")));
+      setNotifications(JSON.parse(localStorage.getItem('notifications')));
     }
 
-    window.addEventListener("notification", refreshView);
+    window.addEventListener('notification', refreshView);
 
-    return () => window.removeEventListener("notification", refreshView);
+    return () => window.removeEventListener('notification', refreshView);
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         {loading && <h3>Loading Notifications...</h3>}
 
         {!loading && notifications.length === 0 && (
@@ -42,25 +42,25 @@ function App() {
         {notifications.map((notif, key) => {
           return (
             <a
-              className="link-to-so"
+              className='link-to-so'
               href={notif.source}
-              rel="noopener noreferrer"
-              target="_blank"
+              rel='noopener noreferrer'
+              target='_blank'
               key={key}
             >
               <p
                 style={{
-                  display: "flex",
-                  justifyContent: "space-between",
+                  display: 'flex',
+                  justifyContent: 'space-between',
                   margin: 0,
                   fontSize: 12,
-                  color: "hsl(220, 13%, 68%)",
+                  color: 'hsl(220, 13%, 68%)',
                 }}
               >
                 <span>{notif.type}</span>
                 <span>{notif.created}</span>
               </p>
-              <p className="App-link" style={{ fontSize: 14, margin: 0 }}>
+              <p className='App-link' style={{ fontSize: 14, margin: 0 }}>
                 {notif.title}
               </p>
               <p style={{ fontSize: 12, margin: 0 }}>{notif.comment}</p>
